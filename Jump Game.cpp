3f4aa0f220d00da5074f4e5d28328467c3f309cb
Paras Jain <135434591+ParasJain19@users.noreply.github.com>
2024-08-10 16:05:20 +0530

    2 Approaches LeetCode 55

--- Jump Game.cpp
+++ Jump Game.cpp
@@ -12,3 +12,35 @@ public:
         return false;
     }
 };
+
+// Approach 2
+class Solution {
+public:
+    vector<int> dp;
+    //dp[i] tabhi 1 hoga if it's possible to reach the end from index i, and 0 otherwise.
+    bool dfs(vector<int>& nums, int i) {
+        if (i >= nums.size() - 1) {
+            return true;
+        }
+
+        if (dp[i] != -1) {
+            return dp[i];
+        }
+
+        
+        for (int jump = 1; jump <=nums[i]; jump++)//min 1 jump to chahiye ,atmost nums[i]jumps  
+        {
+            if (dfs(nums, i+jump))//i+ j jumps
+             {
+                return dp[i] = true;
+            }
+        }
+
+        return dp[i] = false;
+    }
+
+    bool canJump(vector<int>& nums) {
+        dp.resize(nums.size(), -1);
+        return dfs(nums, 0);
+    }
+};
