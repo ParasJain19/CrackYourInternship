4a628ddc9f09b8d496c0e1764fdc7118ddde1a14
Paras Jain <135434591+ParasJain19@users.noreply.github.com>
2024-08-16 02:06:16 +0530

    GeeksforGeeks problem

---
+++ Find Pair Given Diff.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    int findPair(int n, int x, vector<int> &arr) {
+        sort(begin(arr),end(arr));
+        int l=0;
+        int r=1;
+        while(r<n)
+        {
+            if(arr[r]-arr[l]==x)
+            return 1;
+            
+            else if(arr[r]-arr[l]<x)
+            r++;
+            
+            else
+            l++;
+            
+            if(l==r)
+            r++;
+        }
+        return -1;
+    }
+};
