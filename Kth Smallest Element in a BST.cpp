a484858a4721239e25426700ba9993c5789589aa
Paras Jain <135434591+ParasJain19@users.noreply.github.com>
2024-08-29 00:30:51 +0530

    LeetCode 230

---
+++ Kth Smallest Element in a BST.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> ans;
+    void dfs(TreeNode* root)
+    {
+        if(root==nullptr)
+        return;
+        dfs(root->left);
+        ans.push_back(root->val);
+        dfs(root->right);
+
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        dfs(root);
+        return ans[k-1];
+    }
+};
